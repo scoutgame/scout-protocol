048b574358139dbec11a730c86897339eeeec9e9
mattcasey <mattwad@gmail.com>
2024-03-14 14:17:48 -0600

    use hardhat ignition

--- __test__/Lock.spec.ts
+++ __test__/Lock.spec.ts
@@ -1,7 +1,7 @@
-import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
-import { time, loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { time, loadFixture } from '@nomicfoundation/hardhat-toolbox-viem/network-helpers';
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { viem } from 'hardhat';
+import { getAddress, parseGwei } from 'viem';
 
 describe('Lock', function () {
   // We define a fixture to reuse the same setup in every test.
@@ -9,18 +9,20 @@ describe('Lock', function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    const lockedAmount = parseGwei('1');
+    const unlockTime = BigInt((await time.latest()) + ONE_YEAR_IN_SECS);
 
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, otherAccount] = await viem.getWalletClients();
 
-    const Lock = await ethers.getContractFactory('Lock');
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const lock = await viem.deployContract('Lock', [unlockTime], {
+      value: lockedAmount
+    });
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    const publicClient = await viem.getPublicClient();
+
+    return { lock, unlockTime, lockedAmount, owner, otherAccount, publicClient };
   }
 
   describe('Deployment', function () {
@@ -33,20 +35,27 @@ describe('Lock', function () {
     it('Should set the right owner', async function () {
       const { lock, owner } = await loadFixture(deployOneYearLockFixture);
 
-      expect(await lock.owner()).to.equal(owner.address);
+      expect(await lock.owner()).to.equal(getAddress(owner.account.address));
     });
 
     it('Should receive and store the funds to lock', async function () {
-      const { lock, lockedAmount } = await loadFixture(deployOneYearLockFixture);
+      const { lock, lockedAmount, publicClient } = await loadFixture(deployOneYearLockFixture);
 
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(lockedAmount);
+      expect(
+        await publicClient.getBalance({
+          address: lock.address
+        })
+      ).to.equal(lockedAmount);
     });
 
     it('Should fail if the unlockTime is not in the future', async function () {
       // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory('Lock');
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith('Unlock time should be in the future');
+      const latestTime = BigInt(await time.latest());
+      await expect(
+        viem.deployContract('Lock', [latestTime], {
+          value: 1n
+        })
+      ).to.be.rejectedWith('Unlock time should be in the future');
     });
   });
 
@@ -55,10 +64,8 @@ describe('Lock', function () {
       it('Should revert with the right error if called too soon', async function () {
         const { lock } = await loadFixture(deployOneYearLockFixture);
 
-        await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet");
+        await expect(lock.write.withdraw()).to.be.rejectedWith("You can't withdraw yet");
       });
-
-      // ...
 
       it('Should revert with the right error if called from another account', async function () {
         const { lock, unlockTime, otherAccount } = await loadFixture(deployOneYearLockFixture);
@@ -66,8 +73,11 @@ describe('Lock', function () {
         // We can increase the time in Hardhat Network
         await time.increaseTo(unlockTime);
 
-        // We use lock.connect() to send a transaction from another account
-        await expect((lock.connect(otherAccount) as typeof lock).withdraw()).to.be.revertedWith("You aren't the owner");
+        // We retrieve the contract with a different account to send a transaction
+        const lockAsOtherAccount = await viem.getContractAt('Lock', lock.address, {
+          client: { wallet: otherAccount }
+        });
+        await expect(lockAsOtherAccount.write.withdraw()).to.be.rejectedWith("You aren't the owner");
       });
 
       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
@@ -76,27 +86,23 @@ describe('Lock', function () {
         // Transactions are sent using the first signer by default
         await time.increaseTo(unlockTime);
 
-        await expect(lock.withdraw()).not.to.be.reverted;
+        await expect(lock.write.withdraw()).to.be.fulfilled;
       });
     });
 
     describe('Events', function () {
       it('Should emit an event on withdrawals', async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(deployOneYearLockFixture);
+        const { lock, unlockTime, lockedAmount, publicClient } = await loadFixture(deployOneYearLockFixture);
 
         await time.increaseTo(unlockTime);
 
-        await expect(lock.withdraw()).to.emit(lock, 'Withdrawal').withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
+        const hash = await lock.write.withdraw();
+        await publicClient.waitForTransactionReceipt({ hash });
 
-    describe('Transfers', function () {
-      it('Should transfer the funds to the owner', async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(deployOneYearLockFixture);
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances([owner, lock], [lockedAmount, -lockedAmount]);
+        // get the withdrawal events in the latest block
+        const withdrawalEvents = await lock.getEvents.Withdrawal();
+        expect(withdrawalEvents).to.have.lengthOf(1);
+        expect(withdrawalEvents[0].args.amount).to.equal(lockedAmount);
       });
     });
   });
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import type { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-toolbox-viem';
+import '@nomicfoundation/hardhat-ignition-viem';
 import 'hardhat-jest'; // Enable support for Jest: https://www.npmjs.com/package/hardhat-jest
 
 const config: HardhatUserConfig = {
