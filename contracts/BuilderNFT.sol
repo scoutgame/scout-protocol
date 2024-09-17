2bf8dc2f642bd57b4aeabb964c3a8433d13e7e9b
motechFR <mo@anyahq.co>
2024-09-17 18:39:18 +0300

    Simplify contract

--- contracts/BuilderNFT.sol
+++ contracts/BuilderNFT.sol
@@ -6,33 +6,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BuilderNFT is ERC1155, Ownable {
 
-    struct BuilderToken {
-        uint256 totalSupply;
-        uint256 price;  // Starting price for the bonding curve
-        uint256 reserve;  // Holds the total funds received from sales
-        address payable proceedsReceiver; // Where the funds go
+    uint256 public nextTokenId;
+    address payable public proceedsReceiver;  // Global proceeds receiver
+    uint256 public priceIncrement;            // Global price increment for all tokens - linear curve
+
+    event BuilderTokenRegistered(uint256 tokenId);
+
+    // Constructor accepts proceedsReceiver and priceIncrement
+    constructor(string memory uri_, address payable _proceedsReceiver, uint256 _priceIncrement) ERC1155(uri_) Ownable(msg.sender) {
+        require(_proceedsReceiver != address(0), "Invalid receiver address");
+        require(_priceIncrement > 0, "Price increment must be greater than zero");
+
+        proceedsReceiver = _proceedsReceiver;
+        priceIncrement = _priceIncrement;
     }
 
-    uint256 public nextTokenId;
-    mapping(uint256 => BuilderToken) public builderTokens; // Maps tokenId to BuilderToken
-
-    event BuilderTokenRegistered(uint256 tokenId, address proceedsReceiver, uint256 price);
-
-    // Pass the URI to the ERC1155 constructor
-    constructor(string memory uri_) ERC1155(uri_) Ownable(msg.sender) {}
-
-    // Register a new builder token
-    function registerBuilderToken(address payable proceedsReceiver, uint256 startPrice) external onlyOwner {
-        require(proceedsReceiver != address(0), "Invalid receiver address");
-
-        builderTokens[nextTokenId] = BuilderToken({
-            totalSupply: 0,
-            price: startPrice,
-            reserve: 0,
-            proceedsReceiver: proceedsReceiver
-        });
-
-        emit BuilderTokenRegistered(nextTokenId, proceedsReceiver, startPrice);
+    // Register a new builder token with an automatically assigned tokenId
+    function registerBuilderToken() external onlyOwner {
+        emit BuilderTokenRegistered(nextTokenId);
         nextTokenId++;
     }
 
@@ -40,50 +31,41 @@ contract BuilderNFT is ERC1155, Ownable {
     function buyToken(uint256 tokenId, uint256 amount) external payable {
         require(amount > 0, "Must buy at least one token");
 
-        BuilderToken storage token = builderTokens[tokenId];
-
-        uint256 cost = getBatchPurchasePrice(tokenId, amount);
+        uint256 cost = getTokenPurchasePrice(tokenId, amount);
 
         require(msg.value == cost, "Incorrect ETH sent");
-
-        token.totalSupply += amount;
-        token.reserve += msg.value;
 
         // Mint the token to the buyer
         _mint(msg.sender, tokenId, amount, "");
 
-        // Transfer the funds to the proceeds receiver
-        token.proceedsReceiver.transfer(msg.value);
+        // Transfer the funds to the global proceeds receiver
+        proceedsReceiver.transfer(msg.value);
     }
 
-    // Update proceeds receiver address
-    function updateProceedsReceiver(uint256 tokenId, address payable newReceiver) external onlyOwner {
+    // Update the global proceeds receiver address
+    function updateProceedsReceiver(address payable newReceiver) external onlyOwner {
         require(newReceiver != address(0), "Invalid address");
-        builderTokens[tokenId].proceedsReceiver = newReceiver;
+        proceedsReceiver = newReceiver;
     }
 
     // Calculate the cost of the tokens using a simple linear bonding curve
-    function getNextPrice(uint256 currentSupply) public pure returns (uint256) {
-        uint256 totalCost = (currentSupply + 1 ) * 0.001 ether; // Linear bonding curve
+    function getNextPrice(uint256 currentSupply) public view returns (uint256) {
+        uint256 totalCost = (currentSupply + 1) * priceIncrement; // Linear bonding curve
         return totalCost;
     }
 
-    function getTokenPurchasePrice(uint256 tokenId) public view returns (uint256) {
-        uint256 currentSupply = builderTokens[tokenId].totalSupply;
-
-        uint256 nextPrice = getNextPrice(currentSupply);
-        
-        return nextPrice;
-    }
-
-    function getBatchPurchasePrice(uint256 tokenId, uint256 amount) public view returns (uint256) {
-        uint256 currentSupply = builderTokens[tokenId].totalSupply;
-
+    // Calculate the total price for a batch of tokens based on bonding curve pricing
+    function getTokenPurchasePrice(uint256 tokenId, uint256 amount) public view returns (uint256) {
+        uint256 currentSupply = totalSupply(tokenId);  // Use totalSupply from ERC1155
         uint256 totalCost = 0;
         for (uint256 i = 0; i < amount; i++) {
             totalCost += getNextPrice(currentSupply + i);
         }
-
         return totalCost;
+    }
+
+    // Use totalSupply from ERC1155 to track how many tokens exist for a specific tokenId
+    function totalSupply(uint256 tokenId) public view returns (uint256) {
+        return balanceOf(address(this), tokenId);  // This checks how many tokens exist (minted)
     }
 }
