a233002b2157d930180cb2434216411daf5872a0
motechFR <mo@anyahq.co>
2024-11-07 13:21:09 +0200

    Integrate claiming with merkle tree proofs from core

--- __test__/contracts/Protocol/ProtocolImplementation/write.spec.ts
+++ __test__/contracts/Protocol/ProtocolImplementation/write.spec.ts
@@ -1,21 +1,51 @@
+import type { ProvableClaim } from '@charmverse/core/protocol';
+import { generateMerkleTree, getMerkleProofs } from '@charmverse/core/protocol';
+
 import { type ProtocolTestFixture } from '../../../deployProtocol';
 import type { ProtocolERC20TestFixture } from '../../../deployProtocolERC20Token';
 import { loadProtocolFixtures } from '../../../fixtures';
-import { walletFromKey, type GeneratedWallet } from '../../../generateWallets';
+import { generateWallets, type GeneratedWallet } from '../../../generateWallets';
 
 describe('ProtocolImplementation', function () {
   let protocol: ProtocolTestFixture;
   let token: ProtocolERC20TestFixture;
   let admin: GeneratedWallet;
   let user: GeneratedWallet;
+
+  let userClaim: ProvableClaim;
+
+  let merkleTree: ReturnType<typeof generateMerkleTree>;
+  let proofs: ReturnType<typeof getMerkleProofs>;
+
+  let claims: ProvableClaim[];
+
   const week = '2024-W41';
-  const amount = 100;
-  const proofs = [
-    '0x88fef743eb2ba108c1ffe0641f5a75074645a3dbac802311e64110fe3ee522b7',
-    '0x72857e15059e88e5883a147e689a03b9d4cd1df50b1edcaef92cc56e279a7677',
-    '0xaecdc66c7cdd3464311f6e34612ea80b6bcdf07555ddf5e9e9481ef40a1894b0'
-  ] as const;
-  const rootHash = '9de37c56d9eeb93e34fd8764b168879ee52d3559abeb943d9d730efe002c52cc';
+
+  beforeAll(async () => {
+    const { userAccount, secondUserAccount, thirdUserAccount } = await generateWallets();
+
+    user = userAccount;
+
+    userClaim = {
+      address: userAccount.account.address,
+      amount: 100
+    };
+
+    claims = [
+      userClaim,
+      {
+        address: secondUserAccount.account.address,
+        amount: 200
+      },
+      {
+        address: thirdUserAccount.account.address,
+        amount: 300
+      }
+    ];
+
+    merkleTree = await generateMerkleTree(claims);
+    proofs = getMerkleProofs(merkleTree.tree, claims[0]);
+  });
 
   beforeEach(async () => {
     const fixtures = await loadProtocolFixtures();
@@ -24,26 +54,21 @@ describe('ProtocolImplementation', function () {
     protocol = fixtures.protocol;
     admin = protocol.protocolAdminAccount;
 
-    user = await walletFromKey({
-      key: '57b7b9b29419b66ac8156f844a7b0eb18d94f729699b3f15a3d8817d3f5980a3',
-      initialEthBalance: 1
-    });
-
     await token.mintProtocolERC20To({ account: protocol.protocolContract.address, amount: 100_000 });
 
-    await protocol.protocolContract.write.setMerkleRoot([week, `0x${rootHash}`]);
+    await protocol.protocolContract.write.setMerkleRoot([week, `0x${merkleTree.rootHash}`]);
   });
 
   describe('claim', function () {
     describe('effects', function () {
       it('allows a user to claim tokens correctly', async function () {
-        await protocol.protocolContract.write.claim([week, BigInt(amount), proofs], {
+        await protocol.protocolContract.write.claim([week, BigInt(userClaim.amount), proofs], {
           account: user.account
         });
 
         const balance = await token.balanceOfProtocolERC20({ account: user.account.address });
 
-        expect(balance).toEqual(amount);
+        expect(balance).toEqual(userClaim.amount);
 
         const claimed = await protocol.protocolContract.read.hasClaimed([week, user.account.address]);
         expect(claimed).toBe(true);
@@ -52,12 +77,12 @@ describe('ProtocolImplementation', function () {
 
     describe('validations', function () {
       it('denies claims if user has already claimed', async function () {
-        await protocol.protocolContract.write.claim([week, BigInt(amount), proofs], {
+        await protocol.protocolContract.write.claim([week, BigInt(userClaim.amount), proofs], {
           account: user.account
         });
 
         await expect(
-          protocol.protocolContract.write.claim([week, BigInt(amount), proofs], {
+          protocol.protocolContract.write.claim([week, BigInt(userClaim.amount), proofs], {
             account: user.account
           })
         ).rejects.toThrow('You have already claimed for this week.');
@@ -69,15 +94,15 @@ describe('ProtocolImplementation', function () {
           '0x22fef743eb2ba923c1ffe0641f5a75074645a3dbac802311e64110fe3ee522b7'
         ];
         await expect(
-          protocol.protocolContract.write.claim([week, BigInt(amount), invalidProofs as any])
+          protocol.protocolContract.write.claim([week, BigInt(userClaim.amount), invalidProofs as any])
         ).rejects.toThrow('Invalid Merkle proof.');
       });
 
       it('reverts when merkle root is not set', async function () {
         const newWeek = '2024-W55';
-        await expect(protocol.protocolContract.write.claim([newWeek, BigInt(amount), proofs as any])).rejects.toThrow(
-          'Merkle root for this week is not set.'
-        );
+        await expect(
+          protocol.protocolContract.write.claim([newWeek, BigInt(userClaim.amount), proofs as any])
+        ).rejects.toThrow('Merkle root for this week is not set.');
       });
 
       it('reverts when contract balance is insufficient', async function () {
@@ -99,18 +124,18 @@ describe('ProtocolImplementation', function () {
   describe('setMerkleRoot', function () {
     describe('effects', function () {
       it('allows admin to set merkle root correctly', async function () {
-        await protocol.protocolContract.write.setMerkleRoot([week, `0x${rootHash}`]);
+        await protocol.protocolContract.write.setMerkleRoot([week, `0x${merkleTree.rootHash}`]);
 
         const merkleRoot = await protocol.protocolContract.read.getMerkleRoot([week]);
 
-        expect(merkleRoot).toEqual(`0x${rootHash}`);
+        expect(merkleRoot).toEqual(`0x${merkleTree.rootHash}`);
       });
     });
 
     describe('permissions', function () {
       it('reverts when not called by admin', async function () {
         await expect(
-          protocol.protocolContract.write.setMerkleRoot([week, `0x${rootHash}`], {
+          protocol.protocolContract.write.setMerkleRoot([week, `0x${merkleTree.rootHash}`], {
             account: user.account
           })
         ).rejects.toThrow('Proxy: caller is not the claim manager');
@@ -121,13 +146,13 @@ describe('ProtocolImplementation', function () {
           account: protocol.protocolAdminAccount.account
         });
 
-        await protocol.protocolContract.write.setMerkleRoot([week, `0x${rootHash}`], {
+        await protocol.protocolContract.write.setMerkleRoot([week, `0x${merkleTree.rootHash}`], {
           account: user.account
         });
 
         const merkleRoot = await protocol.protocolContract.read.getMerkleRoot([week]);
 
-        expect(merkleRoot).toEqual(`0x${rootHash}`);
+        expect(merkleRoot).toEqual(`0x${merkleTree.rootHash}`);
       });
     });
   });
